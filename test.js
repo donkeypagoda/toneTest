7d52b394df1f34d391f73b6e62a97568ba0b3ebb
donkeypagoda <christopherallenstewart@gmail.com>
2017-10-26 16:53:09 -0700

    messing with polysyth

--- test.js
+++ test.js
@@ -85,11 +85,44 @@ synVol.oninput = () => {
   monoSyn.volume.value = parseFloat(synVol.value);
   console.log(monoSyn.volume.value);
 }
+let gong = new Tone.PolySynth ( [ polyphony = 3], [voice = Tone.MonoSynth]);
+gong.voices[0] =  {
+                    frequency : C4 ,
+                    detune : 0 ,
+                    oscillator : {
+                    type : square
+                    }
+                    ,
+                    filter : {
+                    Q : 6 ,
+                    type : lowpass ,
+                    rolloff : -24
+                    }
+                    ,
+                    envelope : {
+                    attack : 0.005 ,
+                    decay : 0.1 ,
+                    sustain : 0.9 ,
+                    release : 1
+                    }
+                    ,
+                    filterEnvelope : {
+                    attack : 0.06 ,
+                    decay : 0.2 ,
+                    sustain : 0.5 ,
+                    release : 2 ,
+                    baseFrequency : 200 ,
+                    octaves : 7 ,
+                    exponent : 2
+                    }
+                  }
+console.log(gong);
+
 
 
 let ringMyBell = () =>{
   // cymbal.triggerAttack()
   // drum.triggerAttack(pitch)
-  monoSyn.triggerAttackRelease(monoPitch, 0.1)
+  // monoSyn.triggerAttackRelease(monoPitch, 0.1)
 }
 let setIntervalID = setInterval(ringMyBell, 1000)
