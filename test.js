2e5758b362308b7829fefcf58e619829bb0869fd
donkeypagoda <christopherallenstewart@gmail.com>
2017-10-26 14:02:12 -0700

    trying out the membrane synth

--- test.js
+++ test.js
@@ -1,26 +1,50 @@
-let gong = new Tone.MetalSynth().toMaster()
+let cymbal = new Tone.MetalSynth().toMaster()
 let harm = document.getElementById("harmonicity")
 harm.oninput = () => {
-  gong.harmonicity = parseFloat(harm.value);
-  console.log(gong.harmonicity);
+  cymbal.harmonicity = parseFloat(harm.value);
+  console.log(cymbal.harmonicity);
 }
 let freq = document.getElementById("frequency")
 freq.oninput = () => {
-  gong.frequency = parseFloat(freq.value);
-  console.log(gong.frequency);
+  cymbal.frequency = parseFloat(freq.value);
+  console.log(cymbal.frequency);
 }
 let octave = document.getElementById("octaves")
 octave.oninput = () => {
-  gong.octaves = parseFloat(octave.value);
-  console.log(gong.octaves);
+  cymbal.octaves = parseFloat(octave.value);
+  console.log(cymbal.octaves);
 }
 let resonance = document.getElementById("resonance")
 resonance.oninput = () => {
-  gong.resonance = parseFloat(resonance.value);
-  console.log(gong.resonance);
+  cymbal.resonance = parseFloat(resonance.value);
+  console.log(cymbal.resonance);
 }
+
+let drum = new Tone.MembraneSynth().toMaster();
+let env = document.getElementById("envelope")
+env.oninput = () => {
+  drum.envelope = parseFloat(env.value);
+  console.log(drum.envelope);
+}
+let osc = document.getElementById("oscillator")
+osc.oninput = () => {
+  drum.oscillator = parseFloat(osc.value);
+  console.log(drum.oscillator);
+}
+let oct = document.getElementById("oct")
+oct.oninput = () => {
+  drum.octaves = parseFloat(oct.value);
+  console.log(drum.octaves);
+}
+let pDecay = document.getElementById("decay")
+pDecay.oninput = () => {
+  drum.pitchDecay = parseFloat(pDecay.value);
+  console.log(drum.pitchDecay);
+}
+
+
 let ringMyBell = () =>{
-  gong.triggerAttack()
+  // cymbal.triggerAttack()
+  drum.triggerAttack()
 }
 let setIntervalID = setInterval(ringMyBell, 1000)
-console.log(gong);
