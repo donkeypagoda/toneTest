248d58723d104c2473aa32150e8e7b2161d969db
donkeypagoda <christopherallenstewart@gmail.com>
2017-10-30 13:45:54 -0700

    wut

--- test.js
+++ test.js
@@ -1,9 +1,11 @@
 let testTone = new Tone.Synth();
 let pingy = new Tone.PingPongDelay(0.2, 0.5);
-pingy.wet.value = 0.3;
-testTone.connect(pingy);
-// testTone.toMaster()
-pingy.toMaster();
+let verb = new Tone.Freeverb(0.8, 3000);
+pingy.wet.value = 0.6;
+testTone.connect(verb);
+// pingy.connect(verb);
+verb.toMaster();
+// pingy.toMaster();
 
 
 let ringMyBell = () =>{
@@ -12,5 +14,5 @@ let ringMyBell = () =>{
   // monoSyn.triggerAttackRelease(monoPitch, 0.1)
   testTone.triggerAttackRelease(200, 0.1)
 }
-let setIntervalID = setInterval(ringMyBell, 1000)
+let setIntervalID = setInterval(ringMyBell, 2000)
 // ringMyBell()
